8aeba56b91aa766c220c44a86b476e09690b62f5
Jason Kautz <jkautz@launchpadcentral.com>
2015-09-03 14:51:20 -0700

    added httprouter hello, name test

--- jason/httprouter/server.go
+++ jason/httprouter/server.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
 }
@@ -12,4 +19,6 @@ func main() {
 	router := httprouter.New()
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
